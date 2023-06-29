c4e021a58271b41a1f75964f3f778a856c6864b4
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-06-28 20:27:06 -0500

    added basic onclick functionality

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,3 +1,25 @@
-export default function main() {
-  return <img src="../assets/photo-tag.jpg" alt="Background Photo" />;
+import "./main.css";
+
+const background = require("../assets/photo-tag.jpg");
+
+export default function Main() {
+  const target = (e: React.MouseEvent<HTMLElement>): void => {
+    e.preventDefault();
+    const mouseX: number = e.pageX;
+    const mouseY: number = e.pageY;
+    const targetDiv: HTMLDivElement = document.createElement("div");
+    targetDiv.style.position = "relative";
+    targetDiv.style.left = `${mouseX}px`;
+    targetDiv.style.top = `${mouseY}px`;
+    targetDiv.style.border = "2px solid red";
+    targetDiv.textContent = "30px";
+    const img = document.querySelector("img") as HTMLElement;
+    img.appendChild(targetDiv);
+  };
+
+  return (
+    <>
+      <img src={background} alt="Video Game Characters" onClick={target} />
+    </>
+  );
 }
